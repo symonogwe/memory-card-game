c95c6632fd181d8c2785c940d741e1ce6b86673f
Symon Ogwe <symonogwe@gmail.com>
2024-01-08 10:36:20 +0300

    Use score props & display them in component

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,12 +1,12 @@
-export default function Nav() {
+export default function Nav({ score, bestScore }) {
   return (
     <div className="nav-container">
       <div className="game-name-container">
         <h1>Rick & Morty Memory Game</h1>
       </div>
       <div className="score-board-container">
-        <p>Score:</p>
-        <p>Best Score:</p>
+        <p>Score: {score}</p>
+        <p>Best Score: {bestScore}</p>
       </div>
     </div>
   );
