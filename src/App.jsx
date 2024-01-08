e26f879672402cde63a3b246c864bfe7121f729b
Symon Ogwe <symonogwe@gmail.com>
2024-01-08 10:35:43 +0300

    Create state for score & best score

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import MainCard from "./MainCard";
 import Nav from "./Nav";
 
+import { useState } from "react";
+
 export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  function increaseScore() {
+    setScore((score) => score + 1);
+  }
+
+  function increaseBestScore() {
+    setBestScore((bestScore) => bestScore + 1);
+  }
+
   return (
     <div className="maim-container">
-      <Nav />
-      <MainCard />
+      <Nav score={score} bestScore={bestScore} />
+      <MainCard
+        increaseScore={increaseScore}
+        increaseBestScore={increaseBestScore}
+      />
     </div>
   );
 }
