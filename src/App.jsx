7d1beb5d1cd254835a06289af003e02ef59acd25
Symon Ogwe <symonogwe@gmail.com>
2024-01-09 12:07:41 +0300

    Add info paragraph to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ export default function App() {
   return (
     <div className="main-container">
       <Nav score={score} bestScore={bestScore} />
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </p>
       <MainCard
         setScore={setScore}
         setBestScore={setBestScore}
